5acbfa65b5a5450eb90b2f78d57524b433fa739d
siasColin <1540247870@qq.com>
2019-08-01 15:02:32 +0800

    初始化提交

---
+++ src/com/how2java/TestAnalyzer.java
@@ -0,0 +1,19 @@
+package com.how2java;
+
+import java.io.IOException;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.wltea.analyzer.lucene.IKAnalyzer;
+
+public class TestAnalyzer {
+
+	public static void main(String[] args) throws IOException {
+		
+		IKAnalyzer analyzer = new IKAnalyzer();
+		TokenStream ts= analyzer.tokenStream("name", "护眼带光源");
+		ts.reset();
+		while(ts.incrementToken()){
+			System.out.println(ts.reflectAsString(false));
+		}
+	}
+}
